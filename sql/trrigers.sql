bacaba447f47db08bdf97136b9ab08939d1582ac
iyyappanvasu <iyyappanvasu03@gmail.com>
2025-06-12 16:58:35 +0530

    updated sqly

---
+++ sql/trrigers.sql
@@ -0,0 +1,5 @@
+-- Update number of lines in NEXTUPNUMBER table
+UPDATE NEXTUPNUMBER SET NUMBEROFLINES = 10;
+
+-- Update PREFIX to empty (use single quotes, not double)
+UPDATE NEXTUPNUMBER SET PREFIX = 'ASN';
